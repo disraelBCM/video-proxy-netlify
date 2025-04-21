8b7655e709a0477238832615dd21cf11408531f1
disraelBCM <122294362+disraelBCM@users.noreply.github.com>
2025-04-21 16:07:29 -0400

    Update getVideo.js

--- netlify/functions/getVideo.js
+++ netlify/functions/getVideo.js
@@ -1,16 +1,7 @@
-export async function handler(event, context) {
-  const { id, site } = event.queryStringParameters;
-  const url = `https://tableaureportfunctions.azurewebsites.net/api/PlaylistManifestRequest?code=YOUR-REAL-CODE&id=${id}&site=${site}`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.text();
-    return {
-      statusCode: 200,
-      body: data,
-      headers: { 'Access-Control-Allow-Origin': '*' }
-    };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
-}
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: "Hello from Netlify Function!" })
+  };
+};
